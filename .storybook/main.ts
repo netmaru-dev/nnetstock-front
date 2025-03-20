da55dbdd3170ead38910a09646b47fa000dbbfb1
ayoung-netmaru-dev <ayoung26026@nate.com>
2025-03-20 09:30:42 +0900

    📦 build: Storybook 적용
    Storybook 환경설정 추가

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  "core": {
+    builder: '@storybook/builder-vite',
+  },
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test"
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
+};
+export default config;
