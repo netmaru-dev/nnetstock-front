b0e6348be9898f8fe144b2a729c2ed00ab09453c
ayoung-netmaru-dev <ayoung26026@nate.com>
2025-03-24 09:50:36 +0900

    💄 style: reset.css 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import AppRoutes from './routes';
+import './reset.css';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
