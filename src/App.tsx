5185275eedb11e60680f9047d539dc57dddd1e64
ayoung-netmaru-dev <ayoung26026@nate.com>
2025-03-24 09:52:31 +0900

    🔧 wrench: tailwindCSS 미사용 import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import './common/styles/tailwind.css';
-
 function App() {
   return (
     <>
-      <h1 className="text-2xl text-(--color-primary)">nnetstock</h1>
+      <h1 className='text-primary text-2xl'>nnetstock admin route</h1>
     </>
   );
 }
