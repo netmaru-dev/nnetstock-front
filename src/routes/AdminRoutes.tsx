3a9780f2c7ec3c28f4eda92478c4a8b8a8409f97
ayoung-netmaru-dev <ayoung26026@nate.com>
2025-03-25 10:28:33 +0900

    ✨ feat: 사이트관리 페이지 생성 및 구성

--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import AdminApp from '../admin/AdminApp';
+import SiteManagePage from '@/admin/pages/site/SiteManagePage';
 
 const AdminRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<AdminApp />}>
         <Route index element={<h1>admin main</h1>} />
-        {/* <Route path='' element={} /> */}
+        <Route path='/site' element={<SiteManagePage />} />
       </Route>
     </Routes>
   );
