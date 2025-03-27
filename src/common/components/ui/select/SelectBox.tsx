edeb2e15c42eaaefcb6411bfd9738aa82f64d3d0
ayoung-netmaru-dev <ayoung26026@nate.com>
2025-03-27 16:15:49 +0900

    ✨ feat: 셀렉트박스 컴포넌트 추가가

---
+++ src/common/components/ui/select/SelectBox.tsx
@@ -0,0 +1,33 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+interface SelectBoxProps {
+  value: string;
+  handleValueChange: (value: string) => void;
+  options: { value: string; label: string }[];
+  placeholder?: string;
+}
+
+const SelectBox = ({ value, handleValueChange, options, placeholder = '선택' }: SelectBoxProps) => {
+  return (
+    <Select value={value} onValueChange={handleValueChange}>
+      <SelectTrigger className='h-10 w-full min-w-[100px]'>
+        <SelectValue placeholder={placeholder}>{value}</SelectValue>
+      </SelectTrigger>
+      <SelectContent>
+        {options.map(option => (
+          <SelectItem key={option.value} value={option.value}>
+            {option.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default SelectBox;
