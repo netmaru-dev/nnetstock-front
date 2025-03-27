6aba586a105ce97660f6e00de3c1ba816270ad33
ayoung-netmaru-dev <ayoung26026@nate.com>
2025-03-27 16:15:23 +0900

    ✨ feat: shadcn/ui 컴포넌트 props 수정

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,7 +20,8 @@ const buttonVariants = cva(
       size: {
         default: 'h-9 px-4 py-2',
         sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8 py-6',
+        lg: 'h-10 rounded-md px-8 py-4',
+        big: 'h-12 rounded-md px-10 py-4',
         icon: 'h-9 w-9',
       },
     },
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,7 +9,7 @@ const inputVariants = cva(
       size: {
         default: 'h-9 px-4 py-2 text-base',
         sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8 py-6 text-base',
+        lg: 'h-10 rounded-md px-4 py-4 text-base',
         icon: 'h-9 w-9 p-0 text-base text-center',
       },
     },
