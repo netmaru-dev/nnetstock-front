155be46ce819ac1d616877b6267019c7d403ac48
ayoung-netmaru-dev <ayoung26026@nate.com>
2025-03-27 13:49:41 +0900

    ✨ feat: shadcn/ui 아코디언, 버튼, 모달, 페이지네이션 컴포넌트 추가가

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,6 +58,28 @@ export default {
           5: 'hsl(var(--chart-5))',
         },
       },
+      keyframes: {
+        'accordion-down': {
+          from: {
+            height: '0',
+          },
+          to: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+        },
+        'accordion-up': {
+          from: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+          to: {
+            height: '0',
+          },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
     colors: {
       transparent: 'transparent',
@@ -83,9 +105,9 @@ export default {
       24: '1.5rem',
       28: '1.75rem',
     },
-    maxWidth: {
-      DEFAULT: '1440px',
-    },
+    // maxWidth: {
+    //   DEFAULT: '1440px',
+    // },
     screens: {
       xs: '370px',
       sm: '480px',
