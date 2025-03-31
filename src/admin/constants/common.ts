860d1d3e1e2f566fa5dd7283044da5b88fb4b32d
ayoung-netmaru-dev <ayoung26026@nate.com>
2025-03-31 18:40:22 +0900

    ✨ feat: 공통, 멤버, 메뉴 상수 정의

---
+++ src/admin/constants/common.ts
@@ -0,0 +1,12 @@
+export const PAGE_NUM = 1;
+export const PAGE_SIZE = 10;
+
+export const STATUS = {
+  INACTIVE: '0',
+  ACTIVE: '1',
+};
+
+export const STATUS_LABEL = {
+  [STATUS.INACTIVE]: '사용안함',
+  [STATUS.ACTIVE]: '사용함',
+};
